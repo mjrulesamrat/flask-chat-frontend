e9b8d53efac89d7bd0d98e67b12e3d22b6c2d4e1
Jay Modi <mjrulesamrat@gmail.com>
2018-12-17 20:53:35 +0530

    Update index page design
    Install bootstrap and reactstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import { Button } from 'reactstrap';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Flask chat frontend
-          </p>
-          <p>Work in progress..</p>
-        </header>
-      </div>
+    <span>Work in progress..</span>
     );
   }
 }
